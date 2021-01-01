787908c71c575d0e9b2af4442f0337d82c2156d1
bhbs <bhbstar.me@gmail.com>
2021-01-01 10:55:45 +0900

    introduce ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,17 +23,11 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string })
     }
   })
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  })
+  return allPostsData.sort((a, b) => (a.date < b.date) ? 1 : -1)
 }
 
 export function getAllPostIds() {
@@ -64,6 +58,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; titlee: string })
   }
 }
