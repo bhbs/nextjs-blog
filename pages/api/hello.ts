e9644ef99b7182acf39ba43c94315b6984c1b1c2
bhbs <bhbstar.me@gmail.com>
2021-01-21 00:19:30 +0900

    update mail function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,25 +5,27 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const info = req.query?.text ? await sendText(req.query.text) : false;
-  res.status(200).json(info || { text: req.query || "Hello" });
+  const info = req.query.mail ? await sendText(req.query) : false;
+  res.status(200).json(info || { text: req.query || "mail & subject & text" });
 };
 
-const sendText = async (text): Promise<nodemailer.SentMessageInfo> => {
+const sendText = async (params: {
+  [key: string]: string | string[];
+}): Promise<nodemailer.SentMessageInfo> => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
       user: "dancingbumpkin@gmail.com",
-      pass: "whuemesyvgabhrrt",
+      pass: process.env.GMAIL_APPLICATION_PW,
     },
   });
 
   return await transporter.sendMail({
-    from: '"mailmaster"<dancingbumpkin@gmail.com>',
+    from: '"dancingbumpkin"<dancingbumpkin@gmail.com>',
     to: "dancingbumpkin@gmail.com",
-    subject: "Hello ✔",
-    text,
+    subject: JSON.stringify(params.subject),
+    text: JSON.stringify(params.text),
   });
 };
