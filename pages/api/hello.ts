413999e1b9fa533c1f082f6c45d73e7299434299
bhbs <bhbstar.me@gmail.com>
2021-01-20 23:59:45 +0900

    add mail function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import * as nodemailer from "nodemailer";
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(200).json({ text: req.query || "Hello" });
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const info = req.query?.text ? await sendText(req.query.text) : false;
+  res.status(200).json(info || { text: req.query || "Hello" });
+};
+
+const sendText = async (text): Promise<nodemailer.SentMessageInfo> => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "dancingbumpkin@gmail.com",
+      pass: "whuemesyvgabhrrt",
+    },
+  });
+
+  return await transporter.sendMail({
+    from: '"mailmaster"<dancingbumpkin@gmail.com>',
+    to: "dancingbumpkin@gmail.com",
+    subject: "Hello ✔",
+    text,
+  });
 };
