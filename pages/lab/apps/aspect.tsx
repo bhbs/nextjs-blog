5e3908f0b2134c7ddcfc3a20f9a7bc415edf7061
bhbs <bhbstar.me@gmail.com>
2021-02-02 22:44:12 +0900

    add aspect calculator

---
+++ pages/lab/apps/aspect.tsx
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import Layout from "../../../components/layout";
+import styles from "./aspect.module.scss";
+
+const Lab: React.FC = (): React.ReactElement => {
+  const [result, setResult] = useState([0, 0]);
+  const [heightUnit, setHeightUnit] = useState(0);
+  const [widthUnit, setWidthUnit] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const result =
+      height / heightUnit > width / widthUnit
+        ? [width * (heightUnit / widthUnit), width]
+        : [height, height * (widthUnit / heightUnit)];
+    setResult(result);
+  }, [height, width]);
+
+  return (
+    <Layout>
+      <Head>
+        <title>Aspect ratio calculator</title>
+      </Head>
+      <section className={styles.aspect}>
+        <p>
+          <input
+            type="number"
+            placeholder="heightUnit"
+            onChange={(e) => {
+              const valnum = parseInt(e.target.value);
+              setHeightUnit(valnum);
+            }}
+          />
+          :
+          <input
+            type="number"
+            placeholder="widthUnit"
+            onChange={(e) => {
+              const valnum = parseInt(e.target.value);
+              setWidthUnit(valnum);
+            }}
+          />
+        </p>
+        <p>
+          <input
+            type="number"
+            placeholder="height"
+            onChange={(e) => {
+              const valnum = parseInt(e.target.value);
+              setHeight(valnum);
+            }}
+          />
+          :
+          <input
+            type="number"
+            placeholder="width"
+            onChange={(e) => {
+              const valnum = parseInt(e.target.value);
+              setWidth(valnum);
+            }}
+          />
+        </p>
+        <div>height : width</div>
+        <div>
+          {result[0]} : {result[1]}
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default Lab;
