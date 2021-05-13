457e3062283649695c79ebe6c7c59676fcb516b1
bhbs <bhbstar.me@gmail.com>
2021-05-13 22:31:08 +0900

    favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -18,7 +18,10 @@ const Layout: React.FC<Props> = ({
   return (
     <div>
       <Head>
-        <link rel="icon" href={staticPath.favicon_ico} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🍙</text></svg>"
+        />
         <meta name="description" content="diary" />
         <meta
           property="og:image"
